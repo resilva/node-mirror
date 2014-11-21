5ac283f64537d3581821829fcb8df974fdad5f89
Romain <r.esilva@sysdream.com>
2014-11-22 00:22:00 +0100

    Add error management

--- mirror.js
+++ mirror.js
@@ -11,22 +11,36 @@ exports.MirrorClient = MirrorClient = function(device) {
 util.inherits(MirrorClient, EventEmitter);
 
 MirrorClient.prototype._open = function() {
-    this._device = fs.createReadStream(this._device_name);
+    try {
+        this._device = fs.createReadStream(this._device_name);
 
-    this._device.on('readable', function() {
-        this._read();
-    }.bind(this));
+        this._device.on('readable', function() {
+            this._read();
+        }.bind(this));
+
+        this._device.on('error', function(error) { this._device_error(error); }.bind(this));
+    } catch (e) {
+        this._device_error(e);
+    }
+};
+
+MirrorClient.prototype._device_error = function(error) {
+    this.emit('error', error);
 };
 
 MirrorClient.prototype._read = function() {
-    var chunk;
-    while (null !== (chunk = this._device.read(16))) {
-        var data = chunk.toString('hex');
-        var state = data.slice(0, 4);
-        var tag = data.slice(4, -1);
+    try {
+        var chunk;
+        while (null !== (chunk = this._device.read(16))) {
+            var data = chunk.toString('hex');
+            var state = data.slice(0, 4);
+            var tag = data.slice(4, -1);
 
-        if (state == '0201' || state == '0202') {
-            this.emit('tag', tag, state);
+            if (state == '0201' || state == '0202') {
+                this.emit('tag', tag, state);
+            }
         }
+    } catch (e) {
+        this._device_error(e);
     }
 };
--- test.js
+++ test.js
@@ -4,3 +4,9 @@ var test = new MirrorClient('/dev/mirror');
 test.on('tag', function(tag, state) {
     console.log('tag "' + tag + '" : ' + state);
 });
+
+test.on('error', function(error) {
+    setTimeout(function() {
+        this._open();
+    }.bind(this), 1000);
+}.bind(test));
